7c8b79c479f2193746bb186a248edf7f4d0b051c
HumbertoJim <humbertodjimz@gmail.com>
2024-01-28 09:56:15 -0500

    Comments correction

--- challengers/python4all/challenge-day-01/solution.py
+++ challengers/python4all/challenge-day-01/solution.py
@@ -5,7 +5,7 @@ import random
 # create a list with random numbers
 my_list = [random.randint(0, 1000) for _ in range(10)]
 
-# conver the list to an array
+# convert the list to an array
 my_array = np.array(my_list)
 
 # print the array
